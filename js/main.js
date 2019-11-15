f788da82b495090d84f953f875c84ed0a336905c
shahsaahab-dev <naqishah2997@gmail.com>
2019-11-16 01:50:07 +0500

    Menu Complete, Structure Complete

--- js/main.js
+++ js/main.js
@@ -12,4 +12,11 @@ jQuery(".overlay").click(function(){
 jQuery(".mobile-menu-btn").click(function(){
     jQuery(".menu-mobile").addClass("open-menu");
     jQuery(".mobile-menu-btn").hide();
+    jQuery(".close-menu-btn").addClass("display-btn");
+});
+
+jQuery(".close-menu-btn").click(function(){
+    jQuery(".menu-mobile").removeClass("open-menu");
+    jQuery(".close-menu-btn").removeClass("display-btn");
+    jQuery(".mobile-menu-btn").show();
 });
