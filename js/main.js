254b36aa156d80c81d8a2aac349222d38536f947
shahsaahab-dev <naqishah2997@gmail.com>
2019-11-16 01:40:34 +0500

    Menu Almost Complete

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Open Overlay when Clicked on SearchBar
+jQuery(".search-custom").click(function(){
+    jQuery(".overlay").addClass("active");
+});
+
+// Close Overlay
+jQuery(".overlay").click(function(){
+    jQuery(".overlay").removeClass("active");
+});
+
+// Open Menu With Click 
+jQuery(".mobile-menu-btn").click(function(){
+    jQuery(".menu-mobile").addClass("open-menu");
+    jQuery(".mobile-menu-btn").hide();
+});
