45df09fb833063ea5e476f8d44c672402d08ab24
shahsaahab-dev <naqishah2997@gmail.com>
2019-11-21 03:58:15 -0800

    More Changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-
+// Console Log Custom Message 
+var text = "Theme Name: OnPoint by Three Cheetahs <br>";
+var text2 = "Developer: Syed Naqi Ali Shah";
+console.log(text + text2);
 // Start Mobile menu Code
 //Open Overlay when Clicked on SearchBar
 jQuery(".search-custom").click(function(){
@@ -37,3 +40,69 @@ jQuery(window).scroll(function() {
         jQuery('.main-navigation').removeClass('nav-active');
     }
 });
+
+
+
+// Slick Slider Product Details Page
+$('.slider-single').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    adaptiveHeight: true,
+    infinite: false,
+   useTransform: true,
+    speed: 400,
+    cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+});
+
+$('.slider-nav')
+    .on('init', function(event, slick) {
+        $('.slider-nav .slick-slide.slick-current').addClass('is-active');
+    })
+    .slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows:true,
+        dots: false,
+        focusOnSelect: false,
+        infinite: false,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 5,
+            }
+        }, {
+            breakpoint: 640,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+           }
+        }, {
+            breakpoint: 420,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+       }
+        }]
+    });
+
+$('.slider-single').on('afterChange', function(event, slick, currentSlide) {
+    $('.slider-nav').slick('slickGoTo', currentSlide);
+    var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+    $('.slider-nav .slick-slide.is-active').removeClass('is-active');
+    $(currrentNavSlideElem).addClass('is-active');
+});
+
+$('.slider-nav').on('click', '.slick-slide', function(event) {
+    event.preventDefault();
+    var goToSingleSlide = $(this).data('slick-index');
+
+    $('.slider-single').slick('slickGoTo', goToSingleSlide);
+});
+
+// Product Variation Buttons 
+jQuery('.variation button').click(function(){
+    jQuery('.variation button').toggleClass('active-variation');
+})
